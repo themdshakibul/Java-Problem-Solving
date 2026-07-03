98bfe6958a1511b44149d91406363ce5e82ab8ac
themdshakibul <mdshakibul88240@gmail.com>
2026-07-03 08:52:01 +0600

    Java Programing

---
+++ Condition/IfElseDemo.java
@@ -0,0 +1,13 @@
+public class IfElseDemo {
+    public static void main(String[] args) {
+
+        //  initialize a variable with int value. if the variable is even the print even.
+
+        int num = 10;
+        if (num % 2 == 0) {
+            System.out.println("Even Number");
+        } else {
+            System.out.println("Odd Number");
+        }
+    }
+}
